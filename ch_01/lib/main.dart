7ee6b4b3475f0c29c3edcb7ee0bd1dd3f14c086e
Sha.gam <gammadov@gmail.com>
2023-02-19 15:57:52 +0300

    added ","

--- ch_01/lib/main.dart
+++ ch_01/lib/main.dart
@@ -8,64 +8,70 @@ class HelloWorldApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "Hello World",
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text("Hello World Travel App"),
-              backgroundColor: Colors.deepPurple,
+      debugShowCheckedModeBanner: false,
+      title: "Hello World",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Hello World Travel App"),
+          backgroundColor: Colors.deepPurple,
+        ),
+        body: Builder(
+          builder: (context) => SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(20),
+              child: Center(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Text(
+                        'Hello World Travel',
+                        style: TextStyle(
+                            fontSize: 26,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue[800]),
+                      ),
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.all(5),
+                      child: Text(
+                        'Discover the World',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.deepPurpleAccent,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Container(
+                          decoration: const BoxDecoration(boxShadow: [
+                            BoxShadow(
+                                color: Colors.blue,
+                                blurRadius: 10,
+                                spreadRadius: 2)
+                          ]),
+                          child: Image.network(
+                            'https://images.freeimages.com/images/large-previews/eaa/the-beach-1464354.jpg',
+                            height: 350,
+                          )),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: ElevatedButton(
+                        child: const Text('Contact Us'),
+                        // onPressed: () => debugPrint('Contact us'),
+                        onPressed: () => contactUs(context),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            body: Builder(
-                builder: (context) => SingleChildScrollView(
-                    child: Padding(
-                        padding: const EdgeInsets.all(20),
-                        child: Center(
-                            child: Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(10),
-                              child: Text(
-                                'Hello World Travel',
-                                style: TextStyle(
-                                    fontSize: 26,
-                                    fontWeight: FontWeight.bold,
-                                    color: Colors.blue[800]),
-                              ),
-                            ),
-                            const Padding(
-                              padding: EdgeInsets.all(5),
-                              child: Text(
-                                'Discover the World',
-                                style: TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.deepPurpleAccent,
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(15),
-                              child: Container(
-                                  decoration: const BoxDecoration(boxShadow: [
-                                    BoxShadow(
-                                        color: Colors.blue,
-                                        blurRadius: 10,
-                                        spreadRadius: 2)
-                                  ]),
-                                  child: Image.network(
-                                    'https://images.freeimages.com/images/large-previews/eaa/the-beach-1464354.jpg',
-                                    height: 350,
-                                  )),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(15),
-                              child: ElevatedButton(
-                                child: const Text('Contact Us'),
-                                // onPressed: () => debugPrint('Contact us'),
-                                onPressed: () => contactUs(context),
-                              ),
-                            ),
-                          ],
-                        )))))));
+          ),
+        ),
+      ),
+    );
   }
 
   void contactUs(BuildContext context) {
