2992dceaa6fda84f2d33df4e54282d12f003168b
Sha.gam <gammadov@gmail.com>
2023-03-11 21:52:44 +0300

    ch_04 updated file null errors

--- ch_04/lib/pong.dart
+++ ch_04/lib/pong.dart
@@ -12,18 +12,17 @@ class Pong extends StatefulWidget {
 }
 
 class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
-
-  double width;
-  double height;
-  double posX;
-  double posY;
-  double batWidth;
-  double batHeight;
+  double? width;
+  double? height;
+  late double posX;
+  late double posY;
+  double? batWidth;
+  double? batHeight;
   double batPosition = 0;
   Direction vDir = Direction.down;
   Direction hDir = Direction.right;
-  Animation<double> animation;
-  AnimationController controller;
+  late Animation<double> animation;
+  late AnimationController controller;
   double increment = 5;
   double randX = 1;
   double randY = 1;
@@ -40,19 +39,18 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
 
     animation = Tween<double>(begin: 0, end: 100).animate(controller);
     animation.addListener(() {
-       setState(() {
-          checkBorders();
-          (hDir == Direction.right)
-              ? posX += ((increment * randX).round())
-              : posX -= ((increment * randX).round());
-          (vDir == Direction.down)
-              ? posY += ((increment * randY).round())
-              : posY -= ((increment * randY).round());
-        });
-
+      setState(() {
         checkBorders();
-        });
-     
+        (hDir == Direction.right)
+            ? posX += ((increment * randX).round())
+            : posX -= ((increment * randX).round());
+        (vDir == Direction.down)
+            ? posY += ((increment * randY).round())
+            : posY -= ((increment * randY).round());
+      });
+
+      checkBorders();
+    });
 
     controller.forward();
   }
@@ -60,36 +58,34 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
-       builder: (BuildContext context, BoxConstraints constraints) {
-        height = constraints.maxHeight;
-        width = constraints.maxWidth;
-        batWidth = width / 5;
-        batHeight = height / 20;
+        builder: (BuildContext context, BoxConstraints constraints) {
+      height = constraints.maxHeight;
+      width = constraints.maxWidth;
+      batWidth = width! / 5;
+      batHeight = height! / 20;
 
-         return Stack(
-           children: <Widget>[
-                         Positioned(
-              top: 0,
-              right: 24,
-              child: Text('Score: ' + score.toString()),
-            ),
-
-             Positioned(
-               child: Ball(),
-               top: posY,
-               left: posX,
-              ),
-              Positioned(
-                bottom: 0,
-                left: batPosition,
-                child:  GestureDetector(
-                    onHorizontalDragUpdate: (DragUpdateDetails update) =>
-                        moveBat(update, context),
-                    child: Bat(batWidth, batHeight))),
-           ],
-         );
-       }
-    );
+      return Stack(
+        children: <Widget>[
+          Positioned(
+            top: 0,
+            right: 24,
+            child: Text('Score: ' + score.toString()),
+          ),
+          Positioned(
+            child: Ball(),
+            top: posY,
+            left: posX,
+          ),
+          Positioned(
+              bottom: 0,
+              left: batPosition,
+              child: GestureDetector(
+                  onHorizontalDragUpdate: (DragUpdateDetails update) =>
+                      moveBat(update, context),
+                  child: Bat(batWidth!, batHeight!))),
+        ],
+      );
+    });
   }
 
   void checkBorders() {
@@ -98,23 +94,23 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
       hDir = Direction.right;
       randX = randomNumber();
     }
-    if (posX >= width - diameter && hDir == Direction.right) {
+    if (posX >= width! - diameter && hDir == Direction.right) {
       hDir = Direction.left;
       randX = randomNumber();
     }
     //check the bat position as well
-     if (posY >= height - diameter - batHeight  && vDir == Direction.down) {
+    if (posY >= height! - diameter - batHeight! && vDir == Direction.down) {
       //check if the bat is here, otherwise loose
-      if (posX >= (batPosition - diameter) && posX <= (batPosition + batWidth + diameter)) {
+      if (posX >= (batPosition - diameter) &&
+          posX <= (batPosition + batWidth! + diameter)) {
         vDir = Direction.up;
         randY = randomNumber();
         setState(() {
-         score++; 
+          score++;
         });
-
       } else {
         controller.stop();
-        showMessage(context);       
+        showMessage(context);
       }
     }
     if (posY <= 0 && vDir == Direction.up) {
@@ -138,34 +134,33 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin {
 
   void showMessage(BuildContext context) {
     showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text('Game Over'),
-          content: Text('Would you like to play again?'),
-          actions: <Widget>[
-            FlatButton(
-              child: Text('Yes'),
-              onPressed: () {
-                setState(() {
-                  posX = 0;
-                  posY = 0;
-                  score = 0; 
-                });
-                Navigator.of(context).pop();
-                controller.repeat();
-              },
-            ),
-            FlatButton (
-              child: Text('No'),
-              onPressed: () {
-                Navigator.of(context).pop();
-                dispose();
-              },)
-          ],
-        );
-      }
-    );
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Game Over'),
+            content: Text('Would you like to play again?'),
+            actions: <Widget>[
+              TextButton(
+                child: Text('Yes'),
+                onPressed: () {
+                  setState(() {
+                    posX = 0;
+                    posY = 0;
+                    score = 0;
+                  });
+                  Navigator.of(context).pop();
+                  controller.repeat();
+                },
+              ),
+              TextButton(
+                child: Text('No'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  dispose();
+                },
+              )
+            ],
+          );
+        });
   }
-
 }
